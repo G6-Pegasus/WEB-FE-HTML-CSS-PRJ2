c76d4173ff259c6808d5fdcb941fb81978df91a1
RDCelin <Rdcelin@uninorte.edu.co>
2024-09-21 16:29:02 -0500

    Added Promotional images

--- src/components/home/PromotionalImage.tsx
+++ src/components/home/PromotionalImage.tsx
@@ -1,9 +1,15 @@
 interface PromotionalImageProps {
-    imageUrl: string
-}
-
-const PromotionalImage = ({} : PromotionalImageProps) => {
-    return <></>
-}
-
-export default PromotionalImage
+    PromotionalIMG: { imageUrl: string }[];
+  }
+  
+  const PromotionalImage = ({ PromotionalIMG }: PromotionalImageProps) => {
+    return (
+      <div className="flex flex-col items-center ">
+        {PromotionalIMG.map((image, index) => (
+          <img key={index} src={image.imageUrl} alt={`Promotional Image ${index + 1}`} className="w-full h-auto" />
+        ))}
+      </div>
+    );
+  };
+  
+  export default PromotionalImage;
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,11 +1,17 @@
 import Header from "./Header"
 import Breadcrumb from './Breadcrumb'
+import PromotionalImage from '../home/PromotionalImage';
 import Footer from "./Footer"
 
 interface MainProps {
     breadcrumbPages: string[],
     children: React.ReactNode
 }
+
+const promotionalImages = [
+    { imageUrl: 'https://i.postimg.cc/0NZpjkw7/prom1.png' },
+    { imageUrl: 'https://i.postimg.cc/Dz7r7J82/prom2.png' },
+  ];
 
 const Main = ({ breadcrumbPages, children } : MainProps) => {
     return <>
@@ -14,6 +20,8 @@ const Main = ({ breadcrumbPages, children } : MainProps) => {
             <Breadcrumb pages={breadcrumbPages} />
             {children}
         </main>
+        <PromotionalImage PromotionalIMG={promotionalImages} />
+        
         <Footer />
     </>
 }
