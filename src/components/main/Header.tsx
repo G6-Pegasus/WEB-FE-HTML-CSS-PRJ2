bd7376297f701134fdd883f107bb916168ec1e6a
caleones <caleones@uninorte.edu.co>
2024-09-22 20:54:20 +0000

    feat/UH2006-create-header: migrated header to a react component and refactored code

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,8 +1,54 @@
 import SubHeader from './SubHeader'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-    return <header className='bg-yellow-400'>
-        Header
+    return <header className='bg-[#211f43] text-white fixed top-0 w-full z-50 md:h-[150px] h-[250px]'>
+        <div className="grid md:grid-cols-3 md:grid-rows-1 grid-cols-2 grid-rows-2 gap-4 p-4">
+            <div className="flex justify-start items-center gap-[10px] col-span-1 row-span-1 md:col-span-1 md:row-span-1 order-1 md:order-1 p-2">
+                <Link className="no-underline text-white h-auto" to="/">
+                    <img className="min-w-[60px] min-h-[60px] w-[60px] h-[60px] rounded-[5px]" src="https://i.postimg.cc/JhsLG9DW/logo.jpg" alt="Logo"></img>
+                </Link>
+                <Link className="no-underline text-white h-auto font-montserrat text-xl font-extrabold sm:block hidden" to="/">
+                    <span className="text-left">
+                        Pegasus Retail Store
+                    </span>
+                </Link>
+            </div>
+
+            <div className="flex justify-center items-center w-full md:w-auto col-span-2 row-span-1 md:col-span-1 md:row-span-1 order-3 md:order-2 p-2 align-middle h-full">
+                <form className="flex justify-center items-center w-full gap-2">
+                    <button className="border-none bg-transparent" type="submit">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "#ffffff", width: "24px", height: "24px"}} />
+                    </button>
+                    <input className="border-none bg-[#eeeeee] text-[16px] font-medium text-black h-[30px] rounded-[5px] w-full md:w-auto gap-8" type="text" placeholder="Buscar..."></input>
+                </form>
+            </div>
+
+            <div className="flex justify-end items-center font-montserrat text-[14px] text-center gap-4 col-span-1 row-span-1 md:col-span-1md:row-span-1 order-2 md:order-3 p-2">
+                <div className="flex justify-center items-center flex-col">
+                    <Link className="no-underline text-white" to="/">
+                    <FontAwesomeIcon icon={faCartShopping} style={{ color: "#ffffff", width: "24px", height: "24px"}} />
+                    </Link>
+                    <Link className="no-underline text-white" to="/">
+                        <span className="header-user_section-area-text">
+                            Carrito
+                        </span>
+                    </Link>
+                </div>
+                <div className="flex justify-center items-center flex-col">
+                    <Link className="no-underline text-white" to="/">
+                    <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff", width: "24px", height: "24px"}} />
+                    </Link>
+                    <Link className="no-underline text-white" to="/">
+                        <span className="header-user_section-area-text">
+                            Tu Cuenta
+                        </span>
+                    </Link>
+                </div>
+            </div>
+        </div>
         <SubHeader/>
     </header>
 }
--- src/components/main/SubHeader.tsx
+++ src/components/main/SubHeader.tsx
@@ -21,7 +21,7 @@ const SubHeader  = () => {
     };
 
     return (
-        <div className="bg-beige p-4">
+        <div className="bg-beige p-0 pl-4">
             <nav>
                 <ul className="flex justify-left gap-4 list-none">
                     {categories.map((category) => (
@@ -31,16 +31,16 @@ const SubHeader  = () => {
                           onMouseEnter={() => handleMouseEnter(category.name)}
                           onMouseLeave={handleMouseLeave}
                         >
-                            <Link to = {`/${category}`} className="font-montserrat text-lg font-medium text-black text-center no-underline">
+                            <Link to = {`/${category}`} className="font-montserrat text-lg font-medium text-white text-center no-underline">
                                 {category.name}
                             </Link>
                             {selectedCategory === category.name && (
-                                <ul className="absolute bg-white list-none p-0 shadow-lg mt-2 z-10">
+                                <ul className="absolute bg-[#211f43] list-none p-0 shadow-lg mt-2 z-10 text-white border-2 border-white">
                                     {category.subcategories.map((subcategory) => (
                                         <li key={subcategory}>
                                             <Link
                                               to = {`/${category.name}/${subcategory}`}
-                                              className="block px-4 py-2 text-sm text-center font-roboto text-black no-underline hover:bg-gray-200"
+                                              className="block px-4 py-2 text-sm text-center font-roboto text-white no-underline hover:bg-gray-200 hover:text-black"
                                             >
                                                 {subcategory}
                                             </Link>
