b6705bf5b607363ac73d406f2ee199ae05635795
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-09-23 10:52:19 -0500

    feat:  Update styles in FeaturedProduct, Breadcrumb, SubHeader, Product, Home, and Plp components

--- src/components/plp/Product.tsx
+++ src/components/plp/Product.tsx
@@ -11,7 +11,7 @@ const Product = ({ imageUrl, name, brand, starts, reviews, price, discount }: In
                 <img src={imageUrl} alt={`${name} ${brand}`} className="w-3/6 h-auto rounded-lg" />
             </section>
 
-            <section className="text-gray-600 md:w-3/6 flex flex-col justify-between p-2">
+            <section className="text-gray-600 md:w-3/6 flex flex-col justify-between p-2 gap-y-1">
                 <h3 className="text-lg text-black font-bold">{name}</h3>
                 <h5 className="text-gray-500 font-bold">{brand}</h5>
                 <h6 className="text-md">
@@ -23,13 +23,13 @@ const Product = ({ imageUrl, name, brand, starts, reviews, price, discount }: In
                         <FontAwesomeIcon key={index} icon={emptyStar} style={{ color: "#FFD43B" }} />
                     ))}
                 </h6>
-                <p>({reviews} reseñas)</p>
-                {discount > 0 && <del className="text-gray-400">{convertNumberToMoney(price)}</del>}
-                <b className="text-[#2b8603]">
+                <p className="text-xs">({reviews} reseñas)</p>
+                {discount > 0 && <del className="text-gray-400 text-sm">{convertNumberToMoney(price)}</del>}
+                <b className="text-[#2b8603] text-base ">
                     {convertNumberToMoney(price * (1 - discount / 100))} 
                     {discount > 0 && <span> (-{Math.round(discount)}%)</span>}
                 </b>
-                <button className="mt-2 bg-[#211f43] text-white py-2 px-4 rounded-md cursor-pointer">
+                <button className="mt-2 text-white py-2 px-4 rounded-md cursor-pointer  transition-colors duration-300 ease-in-out bg-[#211f43] hover:bg-[#2b8603]">
                     <FontAwesomeIcon icon={faCartPlus} /> Comprar ahora
                 </button>
             </section>
