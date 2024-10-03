43c66caffcefd3a275654af9d954d514832de9d8
Katy <jdiazk@uninorte.edu.co>
2024-10-02 19:37:31 -0500

    featuredProduct api consumption added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,57 @@ import FeaturedProduct from "../components/home/FeaturedProduct"
 import Banner from '../components/home/Banner'
 import PromotionalImage from "../components/home/PromotionalImage"
 import * as Interfaces from '../utils/interfaces'
+import { useEffect, useState } from "react";
+
+
+interface Product {
+    id: number
+    imageUrl: string
+    name: string
+    brand: string
+    category: string
+    subCategory: string
+    loadDate: string
+    description: string
+    info: {
+        [key : string ]: string;
+    }
+    starts: number 
+    reviews: number
+    price: number 
+    discount: number
+}
+
+
 
 const Home = () => {
     const data_promotional_images: Interfaces.PromotionalImage[] = []
-    const featuredProducts: Interfaces.Product[] = []
+    const [products, setProductos] = useState<Product[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
 
+
+    useEffect(() => {
+        const fetchProductos = async () => {
+          try {
+            const response = await fetch(
+              "https://web-fe-html-css-prj2-api.onrender.com/featuredProducts"
+            );
+            if (!response.ok) {
+              throw new Error("Error al cargar los productos");
+            }
+            const data = await response.json();
+            setProductos(data);
+          } catch (err) {
+            setError((err as Error).message);
+          } finally {
+            setIsLoading(false);
+          }
+        };
+    
+        fetchProductos();
+      }, []);
+      
     return (
         <Main>
             <div className="flex flex-col items-center ">
@@ -17,7 +63,17 @@ const Home = () => {
 
             <h1 className="text-[#211f43] text-center pt-6 font-bold text-2xl">Productos Destacados</h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6 px-8 py-6 mb-5">
-                {featuredProducts.map(({ id, imageUrl, name, category, subCategory, price, discount }, index) => {
+           
+            {isLoading && (
+                <p>Cargando productos...</p>
+            )}
+            
+            {error && (
+                <p>Error: {error}</p>
+            )}
+
+            {!isLoading && !error && (
+                products.map(({ id, imageUrl, name, category, subCategory, price, discount }, index) => {
                     return (
                         <FeaturedProduct
                             key={index}
@@ -30,7 +86,8 @@ const Home = () => {
                             discount={discount}
                         />
                     )
-                })}
+                })
+            )}
             </div>
             <Banner bannerUrl="https://i.postimg.cc/YC6Ncf2n/banner.png" />
         </Main>
