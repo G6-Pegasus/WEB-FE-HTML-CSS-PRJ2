39f062214fb11ad5d4d957eb274adf0a50cc710e
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-10-03 02:51:52 -0500

    feat(2025): Added skeleton and error.

--- src/pages/pdp.tsx
+++ src/pages/pdp.tsx
@@ -2,13 +2,11 @@ import Ymbi from '../components/pdp/RelatedProducts';
 import Main from "../components/main/Main";
 
 const Pdp = () => {
-    const productId = 143; 
-    console.log('Cargando página de PDP con el productId:', productId);
+    console.log('Cargando página de PDP');
 
     return (
         <Main>
-            <h1>Detalles del Producto {productId}</h1>
-            <Ymbi productId={productId} />
+            <Ymbi />
         </Main>
     );
 };
